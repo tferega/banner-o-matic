83899a4cd75f80138503a85b238a2ca0f6565db3
Tomo Ferega <tomo@element.hr>
2012-11-25 20:44:48 +0100

    Added project "Common".

--- code/java/project/ASCIIBannerBuild.scala
+++ code/java/project/ASCIIBannerBuild.scala
@@ -5,18 +5,30 @@ import Helpers._
 
 object ASCIIBannerBuild extends Build {
   lazy val root = top(Seq(
-    service,
-    client
+    client,
+    common,
+    service
   ))
 
-  lazy val service = project("Service").
-  settings (
-    Default.scalaProject: _*
-  )
-
-  lazy val client = project("Client").
-  settings ((
+  lazy val client = project(
+    "Client",
+    Seq(),
+    Seq(common)
+  ) settings ((
     Publishing.settings ++
     Default.javaProject): _*
+  )
+
+  lazy val common = project("Common").
+  settings (
+    Default.javaProject: _*
+  )
+
+  lazy val service = project(
+    "Service",
+    Seq(),
+    Seq(common)
+  ) settings (
+    Default.scalaProject: _*
   )
 }
